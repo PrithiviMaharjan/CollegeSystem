6a1992fc9397eb6f0ab2cb438bd1c4f5999e5630
prithivi.maharjan18 <prithivi.maharjan18@gmail.com>
2025-04-09 13:00:11 +0545

    image upload url fix

--- main/java/com/collegeapp/util/ImageUtil.java
+++ main/java/com/collegeapp/util/ImageUtil.java
@@ -96,7 +96,7 @@ public class ImageUtil {
 	}
 
 	public String getSavePath(String saveFolder) {
-		return "C:/Users/Prithivi/eclipse-workspace/islington-student/src/main/webapp/resources/images/" + saveFolder
+		return "C:/Users/Prithivi/eclipse-workspace/CollegeApp/src/main/webapp/resources/images/" + saveFolder
 				+ "/";
 	}
 }
